529c6ae262a9a980a98b39ef54dec582ed950b19
Isaac Ressler <isaacressler@hotmail.com>
2021-12-07 00:15:42 -0500

    20201 Day 7 solutions.

--- AdventOfCode/ChallengeSolvers/2021/Challenge2021Day7Solver.swift
+++ AdventOfCode/ChallengeSolvers/2021/Challenge2021Day7Solver.swift
@@ -8,24 +8,57 @@
 import Foundation
 
 struct Challenge2021Day7Solver: ChallengeSolver {
-  static let defaultValue = """
-"""
+  static let defaultValue = "16,1,2,0,4,2,7,1,2,14"
 
   static func solution(number challengeNumber: ChallengeNumber, for input: String) -> String {
-    let lines = components(from: input)
+    let crabs = components(from: input, separators: ",").map({ Int($0)! })
+
     switch challengeNumber {
     case .one:
-      return getAnswer1(given: lines)
+      return getAnswer1(given: crabs)
     case .two:
-      return getAnswer2(given: lines)
+      return getAnswer2(given: crabs)
     }
   }
 
-  static private func getAnswer1(given: [String]) -> String {
-    return ""
+  struct CrabResult {
+    let fuelUsage: Int
+    let index: Int
   }
 
-  static private func getAnswer2(given: [String]) -> String {
-    return ""
+  static private func calculateFuelUsage(given crabs: [Int], using fuelCalculator: (Int) -> Int) -> CrabResult {
+    guard crabs.count > 1 else {
+      fatalError("Not enough crabs provided")
+    }
+    let min = crabs.min()!
+    let max = crabs.max()!
+
+    var index = -1
+    var cost = Int.max
+
+    for i in min...max {
+      var newCost = 0
+      for crab in crabs {
+        newCost += fuelCalculator(abs(crab - i))
+      }
+      if newCost < cost {
+        cost = newCost
+        index = i
+      }
+    }
+
+    return CrabResult(fuelUsage: cost, index: index)
+  }
+
+  static private func getAnswer1(given crabs: [Int]) -> String {
+    let result = calculateFuelUsage(given: crabs, using: { $0 })
+
+    return "It will cost \(result.fuelUsage) to move all crabs to position: \(result.index)"
+  }
+
+  static private func getAnswer2(given crabs: [Int]) -> String {
+    let result = calculateFuelUsage(given: crabs, using: { $0 * ($0 + 1) / 2 })
+
+    return "It will cost \(result.fuelUsage) to move all crabs to position: \(result.index)"
   }
 }
