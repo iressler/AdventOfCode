d8abfd0e8ca4d50691a466f86cf1c9f42824e188
Isaac Ressler <isaacressler@hotmail.com>
2021-12-07 17:25:50 -0500

    2020 Day 6 solutions.

--- AdventOfCode/ChallengeSolvers/2020/Challenge2020Day6Solver.swift
+++ AdventOfCode/ChallengeSolvers/2020/Challenge2020Day6Solver.swift
@@ -9,23 +9,65 @@ import Foundation
 
 struct Challenge2020Day6Solver: ChallengeSolver {
   static let defaultValue = """
+abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b
 """
 
   static func solution(number challengeNumber: ChallengeNumber, for input: String) -> String {
-    let lines = inputComponents(from: input)
+    let groupAnswers = groupedInputComponents(from: input)
+//    print(groupAnswers)
     switch challengeNumber {
     case .one:
-      return getAnswer1(given: lines)
+      return getAnswer1(given: groupAnswers)
     case .two:
-      return getAnswer2(given: lines)
+      return getAnswer2(given: groupAnswers)
     }
   }
 
-  static private func getAnswer1(given: [String]) -> String {
-    return ""
+  static private func getAnswer1(given allAnswers: [[String]]) -> String {
+    var count = 0
+
+    var currAnswers = Set<String.Element>()
+    for groupAnswers in allAnswers {
+      for individualsAnswer in groupAnswers {
+        currAnswers.insert(contentsOf: Array(individualsAnswer))
+      }
+//      print("Found \(currAnswers.count) unique answers: \(currAnswers)")
+      count += currAnswers.count
+      currAnswers.removeAll()
+    }
+    return "\(count)"
   }
 
-  static private func getAnswer2(given: [String]) -> String {
-    return ""
+  static private func getAnswer2(given allAnswers: [[String]]) -> String {
+    var count = 0
+
+    var currAnswers = [String.Element: Int]()
+
+    for groupAnswers in allAnswers {
+      let groupCount = groupAnswers.count
+      for individualsAnswer in groupAnswers {
+        for answer in individualsAnswer {
+          currAnswers.incrementValue(for: answer)
+        }
+      }
+
+      count += currAnswers.compactMap({ $1 == groupCount }).filter({ $0 }).count
+      currAnswers.removeAll()
+    }
+    return "\(count)"
   }
 }
