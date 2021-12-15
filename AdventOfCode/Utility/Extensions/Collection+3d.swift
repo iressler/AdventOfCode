f3f20400fffea8e5ef142061fccab418c4b21698
Isaac Ressler <isaacressler@hotmail.com>
2021-12-15 15:32:49 -0500

    Add 3d support to Point and (Mutable)Collection.

---
+++ AdventOfCode/Utility/Extensions/Collection+3d.swift
@@ -0,0 +1,60 @@
+//
+//  Array+3d.swift
+//  AdventOfCode
+//
+//  Created by Isaac Ressler on 12/15/21.
+//
+
+import Foundation
+
+// 3d collections must be a Z indexed group of X,Y indexed collections.
+extension Collection where Element: Collection, Element.Index == Index, Element.Element: Collection, Element.Element.Index == Index, Index == Int {
+  func fieldDescription(separator: String = " ", xyRotated: Bool = false) -> String {
+    var result = ""
+    let fieldDescriptions = self.map({ $0.fieldDescription(separator: separator, rotated: xyRotated) })
+    for fieldIndex in 0..<fieldDescriptions.count {
+      result.append("\n\(fieldIndex):\n\(fieldDescriptions[fieldIndex])")
+    }
+    return result
+  }
+}
+
+// Supports non mutable collections, e.g. let bindings?
+extension Collection where Element: Collection, Element.Index == Index, Element.Element: Collection, Element.Element.Index == Index, Index == Int {
+  subscript(point: Point) -> Element.Element.Element  {
+    return self[point.z][point.x][point.y]
+  }
+}
+
+extension MutableCollection where Element: MutableCollection, Element.Index == Index, Element.Element: MutableCollection, Element.Element.Index == Index, Index == Int {
+  subscript(point: Point) -> Element.Element.Element  {
+    get {
+      return self[point.z][point.x][point.y]
+    }
+    set {
+      self[point.z][point.x][point.y] = newValue
+    }
+  }
+
+  func pointsAdjacent(to point: Point, includeDiagonals: Bool = false) -> [Point] {
+    var adjacentPoints = self[point.z].pointsAdjacent(to: point, includeDiagonals: includeDiagonals)
+
+    if point.z > 0 {
+      let newPoint = Point(x: point.x, y: point.y, z: point.z-1)
+      adjacentPoints.append(newPoint)
+      if includeDiagonals {
+        adjacentPoints.append(contentsOf: self[newPoint.z].pointsAdjacent(to: newPoint, includeDiagonals: true))
+      }
+    }
+
+    if point.z < (count-1) {
+      let newPoint = Point(x: point.x, y: point.y, z: point.z+1)
+      adjacentPoints.append(newPoint)
+      if includeDiagonals {
+        adjacentPoints.append(contentsOf: self[newPoint.z].pointsAdjacent(to: newPoint, includeDiagonals: true))
+      }
+    }
+
+    return adjacentPoints
+  }
+}
--- AdventOfCode/Utility/Point.swift
+++ AdventOfCode/Utility/Point.swift
@@ -10,6 +10,20 @@ import Foundation
 struct Point {
   let x: Int
   let y: Int
+  let z: Int
+
+  // 2 complete initializers because I'm not sure how to guarantee one doesn't infinite loop instead of calling the other.
+  init(x: Int, y: Int, z: Int = -1) {
+    self.x = x
+    self.y = y
+    self.z = z
+  }
+
+  init(x: Int, y: Int, z: Int? = nil) {
+    self.x = x
+    self.y = y
+    self.z = z ?? -1
+  }
 }
 
 // Allow row/column to be aliases for x/y.
@@ -30,20 +44,28 @@ extension Point: ExpressibleByStringLiteral {
     self.init(pointString: value)!
   }
 
+  // pointString must be formatted "x,y" or "x,y,z", or either of those reversed if rotated is true.
   init?(pointString: String, rotated: Bool = false) {
     let coordinates = pointString.components(separatedBy: ",")
-    guard coordinates.count == 2,
-          let firstStr = coordinates.first,
-          let first = Int(firstStr),
-          let lastStr = coordinates.last,
-          let last = Int(lastStr)
-    else {
+
+    guard let first = Int(coordinates[0]), let second = Int(coordinates[1]) else {
       return nil
     }
-    if rotated {
-      self.init(x: last, y: first)
+    let third: Int?
+    if coordinates.count > 2 {
+      third = Int(coordinates[2])
     } else {
-      self.init(x: first, y: last)
+      third = nil
+    }
+
+    if rotated {
+      if let third = third {
+        self.init(x: third, y: second, z: first)
+      } else {
+        self.init(x: second, y: first)
+      }
+    } else {
+      self.init(x: first, y: second, z: third)
     }
   }
 }
